2613fead1e61dcb633eb03d2394fc3e267209094
Cedric-Jefferson <46280843+Cedric-Jefferson@users.noreply.github.com>
2020-06-09 16:48:51 +0200

    Merch Phase 2

--- mern-voice-record-app/client/demo/src/index.js
+++ mern-voice-record-app/client/demo/src/index.js
@@ -1,3 +1,8 @@
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 import ReactGA from 'react-ga'
@@ -9,14 +14,18 @@ require('./normalize.scss')
 require('./styles/font-awesome/css/all.min.css')
 require('./styles.scss')
 
+//ReactDOM.render(<App />, document.getElementById('root'));
+
+
 export default class Demo extends Component {
   render() {
     return (
       <div>
-        <AudioRecorder/>
+        <App/>
       </div>
     )
   }
 }
 
 render(<Demo />, document.querySelector('#demo'))
+registerServiceWorker();
